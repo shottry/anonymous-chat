358ebe1187e9e8793b6008bcfc257d0cdfe4b9ef
shottry <shottry@gmail.com>
2025-08-12 13:29:54 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
-  }
+    origin: "*",
+  },
 });
 
 let waitingUser = null;
 
-io.on('connection', (socket) => {
-  console.log(`🔌 使用者已連線: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log("使用者連線:", socket.id);
 
   if (waitingUser) {
-    const partner = waitingUser;
+    socket.partner = waitingUser;
+    waitingUser.partner = socket;
+    waitingUser.emit("message", "已找到聊天對象");
+    socket.emit("message", "已找到聊天對象");
     waitingUser = null;
-
-    socket.partner = partner;
-    partner.partner = socket;
-
-    socket.emit('match', { message: '已配對成功！' });
-    partner.emit('match', { message: '已配對成功！' });
   } else {
     waitingUser = socket;
-    socket.emit('waiting', { message: '等待配對中...' });
+    socket.emit("message", "等待其他人加入...");
   }
 
-  socket.on('message', (msg) => {
+  socket.on("message", (msg) => {
     if (socket.partner) {
-      socket.partner.emit('message', msg);
+      socket.partner.emit("message", msg);
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("leave", () => {
+    console.log(`${socket.id} 離開聊天室`);
     if (socket.partner) {
-      socket.partner.emit('partner-disconnected');
+      socket.partner.emit("partner-left");
       socket.partner.partner = null;
     }
-    if (waitingUser === socket) {
-      waitingUser = null;
+    if (waitingUser === socket) waitingUser = null;
+    socket.disconnect(true);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("使用者離線:", socket.id);
+    if (socket.partner) {
+      socket.partner.emit("partner-left");
+      socket.partner.partner = null;
     }
-    console.log(`❌ 使用者離線: ${socket.id}`);
+    if (waitingUser === socket) waitingUser = null;
   });
 });
 
 server.listen(3000, () => {
-  console.log('🚀 Server is running on port 3000');
+  console.log("伺服器已啟動，埠號 3000");
 });
